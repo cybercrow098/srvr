9783ea6b7d44ee6bcf76a013bb0617476bb5d1e3
Authkeeper.dev <127827295+cybercrow098@users.noreply.github.com>
2025-12-27 21:01:16 +0530

    Update Blackists.py

--- Blackists.py
+++ Blackists.py
@@ -19,8 +19,8 @@ from slowapi.middleware import SlowAPIMiddleware
 SECRET_KEY = "super-secret-key-change-this"
 ALGORITHM = "HS256"
 
-LICENSE_START_DATE = datetime(2025, 12, 22)
-LICENSE_VALID_DAYS = 30
+LICENSE_START_DATE = datetime(2025, 12, 27)
+LICENSE_VALID_DAYS = 01
 
 LICENSE_DB: Dict[str, str] = {
     "DMLIB-7X9Q2-AF8KD-M3P7L": "Custom-Dev",
